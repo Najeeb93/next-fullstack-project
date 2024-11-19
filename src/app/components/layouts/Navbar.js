7c1c2fd6bd33a9ce97be7b6bdc2ac6b4140e9e0d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-19 18:49:20 +0500

    add to navbar commit

--- src/app/components/layouts/Navbar.js
+++ src/app/components/layouts/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 
@@ -5,7 +6,9 @@ function Navbar() {
     return (
         <header className="text-white-100 sticky top-0 z-50 bg-gradient-to-r from-indigo-600 via-violet-600 to-orange-600 body-font">
             <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
-            
+            <Link href={"/"} className="flex title-font font-extrabold items-center upparcase text-gray-100">
+            <p className="leading-5 text-xl mx-1">Pizza Wizza</p>
+            </Link>
             </div>
         </header>
     )
