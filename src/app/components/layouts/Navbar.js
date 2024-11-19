6121801936cc2c9cde83d4202e8737f7eb756c50
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-19 18:41:13 +0500

    update navbar

--- src/app/components/layouts/Navbar.js
+++ src/app/components/layouts/Navbar.js
@@ -4,7 +4,9 @@ import React from "react";
 function Navbar() {
     return (
         <header className="text-white-100 sticky top-0 z-50 bg-gradient-to-r from-indigo-600 via-violet-600 to-orange-600 body-font">
+            <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
             
+            </div>
         </header>
     )
 }
