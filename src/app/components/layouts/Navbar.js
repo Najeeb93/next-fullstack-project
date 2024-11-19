2795a37911b9ec05cdd41909b25f79ba004b6402
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-19 19:26:49 +0500

    update navbar image

--- src/app/components/layouts/Navbar.js
+++ src/app/components/layouts/Navbar.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -7,7 +8,8 @@ function Navbar() {
         <header className="text-white-100 sticky top-0 z-50 bg-gradient-to-r from-indigo-600 via-violet-600 to-orange-600 body-font">
             <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
             <Link href={"/"} className="flex title-font font-extrabold items-center upparcase text-gray-100">
-            <p className="leading-5 text-xl mx-1">Pizza Wizza</p>
+            <Image alt="Navbar Logo" src={"/pizza.svg"} width={60} height={60} />
+            <p className="leading-6 text-xl mx-1">Pizza Wizza</p>
             </Link>
             </div>
         </header>
