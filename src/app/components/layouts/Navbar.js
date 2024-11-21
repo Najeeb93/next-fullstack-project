0e3cba9f08fe8ac55cc41bff3a0b7ec32b39f0cb
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-21 18:28:24 +0500

    add to commit

--- src/app/components/layouts/Navbar.js
+++ src/app/components/layouts/Navbar.js
@@ -49,19 +49,19 @@ function Navbar() {
               />
             </svg>
             </Link>
-            <Link href={"/cart"} className="text-white mr-5 cursor-pointer hover:text-gray-200 flex items-center"> Login
+            <Link href={"/login"} className="text-white mr-5 cursor-pointer hover:text-gray-200 flex items-center"> Login
              <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.8}
+              stroke-Width="1.5"
               stroke="currentColor"
-              className="w-5 h-5 mx-1"
+              className="w-6 h-6 mx-1"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
               />
             </svg>
            
