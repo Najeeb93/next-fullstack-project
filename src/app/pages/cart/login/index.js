2f6b682b8631e58656b622415f0813b22b724e74
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-24 18:44:19 +0500

    add to commit login file

--- src/app/pages/cart/login/index.js
+++ src/app/pages/cart/login/index.js
@@ -3,7 +3,9 @@
 
 function Login() {
     return (
-        <div>Login</div>
+        <div style={{height:"90vh", backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize:"cover"}}
+        
+        ></div>
     )
 }
 
