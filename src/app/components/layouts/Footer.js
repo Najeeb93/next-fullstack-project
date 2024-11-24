747c13eca5a979a2051a48a4dce7508eedf5570f
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-24 18:29:57 +0500

    update footer

--- src/app/components/layouts/Footer.js
+++ src/app/components/layouts/Footer.js
@@ -4,7 +4,9 @@ import React from "react";
 function Footer() {
     return (
         <footer className="text-white-100  bg-orange-500 body-font"> 
+        <div className="container mx-auto text-white flex flex-wrap p-3 flex-col md:flex-row items-center">
 
+        </div>
         </footer>
     )
 }
