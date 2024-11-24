a5ad26fbbe6863c289ea3406d73ce8d116652508
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-24 18:32:31 +0500

    add to commit

--- src/app/components/layouts/Footer.js
+++ src/app/components/layouts/Footer.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 
@@ -5,7 +7,13 @@ function Footer() {
     return (
         <footer className="text-white-100  bg-orange-500 body-font"> 
         <div className="container mx-auto text-white flex flex-wrap p-3 flex-col md:flex-row items-center">
-
+        <Link
+          href={"/"}
+          className="flex title-font font-extrabold items-center upparcase text-gray-100"
+        >
+          <Image alt="Navbar Logo" src={"/pizza.svg"} width={60} height={60} />
+          <p className="leading-6 text-xl mx-1">Pizza Wizza</p>
+        </Link>
         </div>
         </footer>
     )
