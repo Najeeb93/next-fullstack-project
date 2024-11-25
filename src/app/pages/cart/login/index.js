04068432b39e8ef33159ad5fe059884968a63f39
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-25 14:41:20 +0500

    update login file

--- src/app/pages/cart/login/index.js
+++ src/app/pages/cart/login/index.js
@@ -4,7 +4,7 @@
 function Login() {
     return (
         <div style={{height:"90vh", backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize:"cover"}}
-        
+        className=""
         ></div>
     )
 }
