eae7110cf2cac8389982a504dd8b713a45723228
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-28 16:58:33 +0500

    update

--- src/app/pages/cart/login/index.js
+++ src/app/pages/cart/login/index.js
@@ -9,7 +9,9 @@ function Login() {
         <div style={{height:"90vh", backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize:"cover"}}
         className="flex justify-center items-center">
             <div className="container w-full max-w-md">
-              <form onSubmit={handleSubmit}></form>  
+              <form onSubmit={handleSubmit}
+              className="bg-grey-100 dark:bg-gray-900 dark:text-gray-100 border-gradient rounded-lg shadow-2xl px-8 pt-6 pb-8 mb-4"
+              ></form>  
             </div>
         </div>
     )
