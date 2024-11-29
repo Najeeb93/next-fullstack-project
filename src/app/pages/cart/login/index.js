71dda6040d8e7eba16e7bcc88552d30aab3c8ae2
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-29 17:28:48 +0500

    update index

--- src/app/pages/cart/login/index.js
+++ src/app/pages/cart/login/index.js
@@ -17,7 +17,9 @@ function Login() {
                     <label htmlFor="email" className="block text-gray-700 dark:text-sm font-bold mb-2">
                     Username
                     </label>
-                    <input name="email" type="text"  />
+                    <input name="email" type="text" 
+                      className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 focus:border-indigo-700 text-gray-700 dark:text-gray-100  leading-tight focus:outline-none focus:shadow-outline"
+                      />
                 </div>
                 </form>  
             </div>
