87462b303e52c62e9d0d892252df2faacdbeac6d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-01 17:42:55 +0500

    update

--- src/app/components/layouts/Layout.js
+++ src/app/components/layouts/Layout.js
@@ -2,12 +2,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Home from "@/app/page";
+import Login from "@/app/pages/cart/login";
 
 
 function Layout({Children}) {
     return (
         <>
         <Navbar/>
+        <Home/>
         {Children}
         <Footer/>
         </>
